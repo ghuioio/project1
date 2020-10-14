27cade07b800291218ef940054a1f2af383d0b3a
ghuioio <54397393+ghuioio@users.noreply.github.com>
2020-10-14 21:30:06 +0700

    Update aimn.cpp

--- aimn.cpp
+++ aimn.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-
 #include<algorithm>
 #include<array>
 #include<string.h>
@@ -34,7 +33,7 @@ string lastchar(vector<string> s, int size)
 
 bool mycomp(string a, string b){
 	return a<b;}
-//////
+
 
 void findkey (string source, vector<string> v){
 	int key;
@@ -47,12 +46,16 @@ void findkey (string source, vector<string> v){
 		}
 	}
 }
-
+void movefront(string s, string a[]){
+	for  (int i=0; i < s.length(); i++){
+		a[i] = s.at(i) + a[i];
+	}
+}
 int main(){
-	int starttest;
-	cout << "Encrypt(Press 1) or Decrypt(Press 2): ";
-	cin >> starttest;
-	if (starttest == 1) {
+	int start;
+	cout << "BWT(Press 1) or RevBWT(Press 2): ";
+	cin >> start;
+	if (start == 1) {
 		cout << "Your text is: ";
 		string text;
 		cin >>text;
@@ -72,9 +75,39 @@ int main(){
 		    cout << v[i] <<endl;
 		}
 		result = lastchar(v,size);
+		cout << endl << "BWT success !" << endl;
 		cout << endl << "bwt = " << result << endl;
 		findkey(text, v);
 
     }
+    if (start == 2) {
+    	cout << "Your text is: "; string text; cin >> text;
+    	cout << "Key is : "; int key; cin >> key;
+    	while ( key < 0 ) {
+    		cout << "Your key must be greater or equal to 0." << endl;
+    		cout << "Key is : "; cin >> key;
+    	}
+    	int size = text.length();
+		string revrotations[size];
+		vector<string> v;
+		for (int cycles = 0; cycles < size; cycles++){
+			movefront(text, revrotations );
+			for (int i=0; i < size; i++){
+				v.push_back(revrotations[i]);
+			}
+			sort(v.begin(), v.end(), mycomp);
+			cout << endl;
+			cout << "Stage " << (cycles+1) << endl;
+			for (int i=0; i < size; i++){
+				revrotations[i] = v[i];
+				cout << revrotations[i] << endl;
+			}
+			v.clear();
 
+		}
+		cout << endl << "RevBWT success !" << endl;
+		cout << "Your key is " << key << endl;
+		cout << "RevBWT  = "<< revrotations[key];
+    }
+	return 0;
 }
