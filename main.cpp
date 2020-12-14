6ab0815cd2dae020ad0165c68b9b81351da9e48e
ghuioio <54397393+ghuioio@users.noreply.github.com>
2020-12-14 20:27:04 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@
 #include"rle.hpp"
 #include"huffman.hpp"
 #include"open.hpp"
-int main(){ //string c = "phamminhkhoi";
-    string c = open_file("a.txt"); cout << c;
-    //c = BWT(c);
-    //c = mtf(c); cout << "mtf:" << c << endl ;
-    //int w; int *weigth = new int[w]; string d ="";
-    //rle(c, weigth, d); //cout <<"rle:"<< c <<endl << endl;
-    string str = buildhuffman(c);
-    enzip(str);
-    c = return_huffman(str);
-    //rev_rle(c, weigth, d);
-   // c = rev_mtf(c); cout <<"rev_mtf:" << c << endl;
-    //c = Rev_BWT(c);
+static string c;
+void nen(){
+    c = open_file("a.txt");
+    c = BWT(c);
+    c = mtf(c);
+    c = buildhuffman(c);
+    c = enzip(c); 
+}
+
+void giai_nen(){
+    c =dezip(c); 
+    c = return_huffman(c); 
+    c = rev_mtf(c);
+    Rev_BWT(c);
+}
+int main(){
+    nen();
+    giai_nen();
     return 0;
 }
