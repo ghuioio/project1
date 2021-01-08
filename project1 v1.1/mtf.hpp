393c530dc8e87acdbcc30b73c2fb62bc956954a3
ghuioio <54397393+ghuioio@users.noreply.github.com>
2021-01-08 15:25:31 +0700

    Update mtf.hpp

--- project1 v1.1/mtf.hpp
+++ project1 v1.1/mtf.hpp
@@ -3,9 +3,8 @@
 #include<iostream>
 
 using namespace std;
-string get_ascii() {
+string ascii() {
 	string ascii;
-
 	for(int i = 32; i < 128; i++) {
 		ascii += char(i);
 	}
@@ -14,28 +13,27 @@ string get_ascii() {
 }
 
 char* mtf(char* arr, int n) {
-    string ascii = get_ascii();
-	char* mtfe = new char[n];
+    string ascii = ascii();
+	char* encode = new char[n];
 	for(int i = 0; i < n; i++) {
 		int loc = ascii.find(arr[i]);
-		mtfe[i] = char(loc);
+		encode[i] = char(loc);
 		if (loc != 0) {
 			ascii = ascii[loc] + ascii.substr(0, loc) + ascii.substr(loc + 1);
 		}
 	}
-	mtfe[n] = '\0';
-	return mtfe;
+	encode[n] = '\0';
+	return encode;
 }
 char* rev_mtf(char* arr, int n) {
-    string ascii = get_ascii();
-	char* mtfd = new char[n];
-
+    string ascii = ascii();
+	char* decode = new char[n];
 	for(int i = 0; i < n; i++) {
-		mtfd[i] = ascii[arr[i]];
+		decode[i] = ascii[arr[i]];
 		ascii = ascii[arr[i]] + ascii.substr(0, arr[i]) + ascii.substr(arr[i] + 1);
 	}
-	mtfd[n] = '\0';
-	return mtfd;
+	decode[n] = '\0';
+	return decode;
 }
 
 
