2c96d812de4c329e7875062fee563a65c86c667e
ghuioio <54397393+ghuioio@users.noreply.github.com>
2021-01-08 10:17:01 +0700

    Create bwt_sa.hpp

---
+++ project1/bwt_sa.hpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void genRotations(string source, string s[] ){
+    s[0] = source;
+	for (unsigned int i = 1; i < source.length(); i++)
+		{
+			s[i] = s[i-1].substr(1);
+			}
+}
+
+string lastchar(string v[], string s[], unsigned int size)
+	{
+		string result = "";
+		for (unsigned int i=0; i < size; i++)
+		{   string bwt1 = v[i];
+			for (unsigned int j = 0; j< size; j++){
+                string bwt2 = s[j]    ;
+				if (bwt2 == bwt1){
+				if (j!=0){result += s[0].at(j-1);}
+				else {result += "?";}}
+			}
+		}
+		return result;
+	}
+
+
+bool mycomp(string a, string b){
+	return a<b;}
+//////
+void movefront(string s, string a[]){
+	for  (unsigned int i=0; i < s.length(); i++){
+		a[i] = s.at(i) + a[i];
+	}
+}
+
+unsigned int find(string text, string a[],unsigned int size){
+    char v = '?';
+    for (unsigned int i = 0; i< size ;i++){
+        if(a[i].at(size-1) == v){
+            size = i; break;
+    }
+} return size;
+}
+
+string BWT(string text){
+		//cout << "Your text is: ";
+		//string text;
+		//cin >>text;
+		text = text + "?";
+		unsigned int size = text.length();
+		string rotations[size];
+		string x[size];
+		string result = "";
+		genRotations(text, rotations);
+
+        cout << endl;
+		vector<string> v;
+		for (unsigned int i=0; i < size; i++){
+		    v.push_back(rotations[i]);
+		}
+
+		sort(v.begin(), v.end(), mycomp);
+		for (unsigned int i=0; i < size; i++){
+            x[i] = v[i] ;
+		}
+		result = lastchar(x, rotations, size);
+		//cout << endl << "bwt = " << result << endl;
+		//v.clear(); delete[] x; delete[] rotations;
+		return result;
+}
+
+void Rev_BWT(string text){
+        unsigned int size = text.length();
+		string revrotations[size];
+		vector<string> v;
+		for (unsigned int cycles = 0; cycles < size; cycles++){
+			movefront(text, revrotations );
+			for (unsigned int i=0; i < size; i++){
+				v.push_back(revrotations[i]);
+			}
+			sort(v.begin(), v.end(), mycomp);
+			for (unsigned int i=0; i < size; i++){
+				revrotations[i] = v[i];
+				//cout << revrotations[i] << endl;
+			}
+			v.clear();
+
+		}
+		//cout << endl << "RevBWT success !" << endl;
+        unsigned int s = find(text, revrotations, size);
+        revrotations[s] = revrotations[s].substr(0, size-1);
+        cout << "RevBWT  = " << revrotations[s] <<endl ;
+        //delete[] revrotations; v.clear();
+
+        ofstream Ghi;
+        Ghi.open("Decode.txt");
+        if (Ghi.fail()){
+		cout << "Failed to open this file!" << endl;
+        }
+        Ghi << revrotations[s] << endl;
+        Ghi.close();
+
+        //cout << "Decode success";
+}
+
+
